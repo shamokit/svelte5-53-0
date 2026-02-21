18da83ae91357514fead18345689cef271fefd85
shamokit <contact@shamokit.com>
2026-02-21 13:10:53 +0900

    🚧 r

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	// 開発サーバー起動時に layerchart を prebundle する（エラーはこの時に発生していた）
-	optimizeDeps: { include: ['layerchart'] }
 });
